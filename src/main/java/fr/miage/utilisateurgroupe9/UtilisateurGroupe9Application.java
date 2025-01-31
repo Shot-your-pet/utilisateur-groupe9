466ff3b821049540788b628beab0da99ba8efb15
Maxime Galaxy Book <max.sevot@gmail.com>
2025-01-31 10:55:12 +0100

    Init spring

---
+++ src/main/java/fr/miage/utilisateurgroupe9/UtilisateurGroupe9Application.java
@@ -0,0 +1,13 @@
+package fr.miage.utilisateurgroupe9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilisateurGroupe9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtilisateurGroupe9Application.class, args);
+    }
+
+}
---
+++ src/test/java/fr/miage/utilisateurgroupe9/UtilisateurGroupe9ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.miage.utilisateurgroupe9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilisateurGroupe9ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
